09b13cced74366386529cb24ec1cb214c8211621
Ali <ali00719@live.com>
2025-06-21 10:04:42 +0530

    Sprigboot project setup and initialized.

---
+++ docmanager/src/test/java/com/main/docmanager/DocmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.docmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
