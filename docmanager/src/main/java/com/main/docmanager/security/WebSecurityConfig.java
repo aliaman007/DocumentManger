c73caddd64ef060e5db90dc4de2ae633a25977be
Ali <ali00719@live.com>
2025-06-22 18:55:32 +0530

    removed unused imports and dependecy

--- docmanager/src/main/java/com/main/docmanager/security/WebSecurityConfig.java
+++ docmanager/src/main/java/com/main/docmanager/security/WebSecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,9 +24,6 @@ public class WebSecurityConfig {
 
     @Autowired
     private JwtFilter jwtFilter;
-
-    @Autowired
-    private UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
